959b6e27f376a565113d843d4a8fe3d62b88de1a
monkey.D Luffy <602110567@qq.com>
2020-08-27 13:01:56 +0800

    redis test

--- Application/Home/Controller/RedisController.class.php
+++ Application/Home/Controller/RedisController.class.php
@@ -39,10 +39,10 @@
 				'age'  => '30'
 			];
 			$redis->set($strCacheKey, json_encode($arrCacheData));
-			$redis->expire($strCacheKey, 30);  # 设置30秒后过期
+			$redis->expire($strCacheKey, 300);  # 设置30秒后过期
 			$json_data = $redis->get($strCacheKey);
 			$data = json_decode($json_data);//获得$data对象
-			//print_r($data->name); //输出数据
+			print_r($data->name); //输出数据
 	
 	
 	
@@ -57,7 +57,7 @@
 			//查看队列
 			$strCount = $redis->lrange($strQueueName, 0, 2);
 			echo "当前队列数据为： <br />";
-			//print_r($strCount);
+			print_r($strCount);
 	
 			//出队列
 			$redis->lpop($strQueueName);
@@ -68,50 +68,7 @@
 			echo "当前队列数据为： <br />";
 			print_r($strCount);
 		}
-	
-	
-		//查询昨天借款、还款数据接口
-		public function search_data(){
-			//$start_time = strtotime(date("Y-m-d"),time())-86400;
-			$start_time = strtotime('2019-10-03 00:00:00');
-			//echo $start_time;die;
-			//$end_time = strtotime(date("Y-m-d"),time());
-			$end_time = strtotime('2019-10-04 00:00:00');
-			//今日借款
-			$where['second_verify_time'] = array('between',array($start_time,$end_time));
-			$reg_list = M('lzh_borrow_info')
-				->field('id,second_verify_time')
-				->where($where)
-				->select();
-			//echo M()->getLastSql();die;
-			if(!empty($reg_list)){
-				$data_reg = '';
-				foreach ($reg_list as $k=>$v){
-					$data_reg .= $v['id']."|";
-				}
-				echo date('Y-m-d',$start_time)."借款标的".$data_reg."<br>";
-			}else{
-				echo date('Y-m-d',$start_time)."无借款标的"."<br>";
-			}
-			//今日还款
-			$status['repayment_time'] = array('between',array($start_time,$end_time));
-			$repay_list = M('lzh_investor_detail id')
-				->distinct(true)
-				->field('borrow_id')
-				->where($status)
-				->select();
-			if(!empty($repay_list)){
-				$data_repay = '';
-				foreach ($repay_list as $k=>$v){
-					$data_repay .= $v['borrow_id']."|";
-				}
-				echo date('Y-m-d',$start_time)."还款标的".$data_repay;
-			}else{
-				echo date('Y-m-d',$start_time)."无还款标的";
-			}
-		
-		}
-		
+
 		
     }
 		
